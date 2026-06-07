4c45808cf167112662cee33794206b5f236993e0
Angela <angelaayodeji@gmail.com>
2026-06-07 02:16:43 -0500

    to do tasks react practice

--- class-components/src/components/ClassInput.jsx
+++ class-components/src/components/ClassInput.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import Count from './Count.jsx';
 
 class ClassInput extends Component {
   constructor(props) {
@@ -7,15 +8,20 @@ class ClassInput extends Component {
     this.state = {
       todos: ['Just some demo tasks', 'As an example'],
       inputVal: '',
+      editVal: '',
+      editIndex: null,
+      isEditing: false,
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.deleteTask = this.deleteTask.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleEditInput = this.handleEditInput.bind(this);
   }
 
   handleInputChange(e) {
-    this.setState((state) => ({
-      ...state,
+    this.setState(() => ({
       inputVal: e.target.value,
     }));
   }
@@ -26,6 +32,33 @@ class ClassInput extends Component {
       todos: state.todos.concat(state.inputVal),
       inputVal: '',
     }));
+  }
+
+  deleteTask(index){
+    this.setState(prevState =>({
+      todos: prevState.todos.filter((_,i) => i != index),
+      editIndex: null,
+      editVal: "",
+    }))
+  }
+
+
+  handleEdit(index){
+    if(this.state.editIndex == index){
+      const newTodos = [...this.state.todos];
+      newTodos[index] = this.state.editVal;
+      this.setState(({
+        editIndex: null, editVal: "", todos: newTodos,
+
+      }))
+      return;
+    }
+    this.setState(({editIndex: index, editVal: this.state.todos[index]}))
+
+  }
+
+  handleEditInput(e){
+    this.setState(({editVal: e.target.value}))
   }
 
   render() {
@@ -43,11 +76,27 @@ class ClassInput extends Component {
           />
           <button type="submit">Submit</button>
         </form>
-        <h4>All the tasks!</h4>
+        <h4>
+          All the tasks!
+          <Count count={this.state.todos.length}/>
+        </h4>
         {/* The list of all the To-Do's, displayed */}
         <ul>
-          {this.state.todos.map((todo) => (
-            <li key={todo}>{todo}</li>
+          {this.state.todos.map((todo, index) => (
+            <li key={index}>
+              {index == this.state.editIndex ? 
+              <input
+              type='text'
+              name='edit-task'
+              value = {this.state.editVal}
+              onChange={this.handleEditInput}
+              ></input> :
+              todo}
+              <button type="button" onClick={() => this.deleteTask(index)}>Delete</button>
+              <button type="button" onClick={()=>this.handleEdit(index)}>
+                {this.state.editIndex == index ? "Resubmit" : "Edit"}
+              </button>
+            </li>
           ))}
         </ul>
       </section>
---
+++ class-components/src/components/Count.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+export default class Count extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <>
+                <div>{this.props.count}</div>
+            </>
+        )
+    }
+}
